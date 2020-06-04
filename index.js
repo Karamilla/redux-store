6d077b1500646bdedaa3c92ddf5da80a887df186
mohammed mustafa <karamillamustafa@gmail.com>
2020-06-04 15:51:47 +0200

    update-the-store

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 function createStore() {
   let state;
+  let listeners = [];
 
   const getState = () => state;
+  const subscribe = (listener) => {
+    listener.push(listener);
+    return () => {
+      listeners = listeners.filter((l) => l !== listener);
+    };
+  };
 
   return {
     getState,
+    subscribe,
   };
 }
+
+const store = createStore();
+store.subscribe(() => {
+  console.log("The new state is : ", store.getState());
+});
